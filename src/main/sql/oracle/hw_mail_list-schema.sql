5796ad0f4e331d6562ad1bbaee58b7206b6feb45
jeyun <hatekies@gmail.com>
2026-09-26 10:23:41 +0900

    Oracle 스키마 COMMENT 이스케이프 수정

--- src/main/sql/oracle/hw_mail_list-schema.sql
+++ src/main/sql/oracle/hw_mail_list-schema.sql
@@ -12,5 +12,5 @@ CREATE TABLE hw_mail_list (
 COMMENT ON TABLE hw_mail_list IS '스팸 게이트웨이 화이트/블랙리스트';
 COMMENT ON COLUMN hw_mail_list.list_type IS 'WHITE 또는 BLACK';
 COMMENT ON COLUMN hw_mail_list.pattern IS '이메일 또는 @도메인 와일드카드';
-COMMENT ON COLUMN hw_mail_list.recipient IS ''*'이면 전역(Oracle은 ''가 NULL이라 센티널 사용), 아니면 특정 수신자 전용 규칙';
+COMMENT ON COLUMN hw_mail_list.recipient IS '"*" 이면 전역(Oracle은 빈 문자열이 NULL이라 센티널 사용), 아니면 특정 수신자 전용 규칙';
 COMMENT ON COLUMN hw_mail_list.reason IS '등록 사유';
